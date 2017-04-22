723bf37ee5f87238af0ad82ca8ae4ed40b16a23a
Tóth András <toth83andras@yahoo.com>
2017-04-22 21:28:28 +0200

    Added backend tests

--- backend/routes.js
+++ backend/routes.js
@@ -1,71 +1,14 @@
 const db = require('./in-memory-db');
-const restify = require('restify');
-const guid = require('guid');
+const Controller = require('./controller');
+
+const controller = new Controller(db);
 
 function setRoutes(server) {
-    server.get('/contacts', (req, res, next) => {
-        res.send(db.records);
-        return next();
-    });
+    server.get('/contacts', controller.getAllContacts);
+    server.get('/contact-details/:userId', controller.getContactDetail);
+    server.get('/search/:name', controller.searchByName);
 
-    server.get('/contact-details/:userId', (req, res, next) => {
-        const userId = new guid(req.params.userId);
-
-        if (guid.isGuid(userId)) {
-            const contact = db.searchById(userId);
-            
-            if (contact) {
-                res.send(contact);
-                return next();
-            }
-        } 
-
-        return next(new restify.NotFoundError(`Record with id [${req.params.userId}] was not found in the database`));
-    });
-
-    server.get('/search/:name', (req, res, next) => {      
-        const contact = db.searchByName(req.params.name);
-        
-        if (contact) {
-            res.send(contact);
-        }
-
-        return next();
-    });
-
-    server.post('/contact/', (req, res, next) => {
-        const minimumNameLength = 3;
-        const maximumNameLength = 200;
-
-        const parsedBody = JSON.parse(req.body);
-
-        const id = new guid(parsedBody.userId)
-        const name = parsedBody.name;
-        const phoneNumber = parsedBody.phoneNumber;
-
-        let validated = false;
-
-        if (!guid.isGuid(parsedBody.userId)) {
-            return next(new restify.BadRequestError(`Bad user id format [${parsedBody.userId}]`));
-        }
-
-        if (!name || name.length < minimumNameLength || name.length > maximumNameLength) {
-            return next(new restify.BadRequestError(`Name should be ${minimumNameLength}-${maximumNameLength} long.`));
-        }
-
-        if (!phoneNumber) {
-            return next(new restify.BadRequestError('Phone number cannot be empty.'));
-        }
-
-        db.addContact({
-            id: id,
-            name,
-            phoneNumber
-        });
-
-        res.send(201);
-        return next();
-    });
+    server.post('/contact/', controller.addContact);
 }
 
 module.exports = setRoutes;
---
+++ backend/test/controller.test.js
@@ -0,0 +1,193 @@
+const expect = require('expect');
+const sinon = require('sinon');
+const restify = require('restify');
+
+const Controller = require('../controller');
+
+describe('Controller', function () {
+  const validGuid = '99ae8275-61a3-5b3b-b790-f7f122e16148';
+
+  let res;
+  let req;
+  let next;
+  let db;
+
+  let controller;
+
+  beforeEach(function () {
+    res = { send: sinon.spy() };
+    req = sinon.stub();
+    next = sinon.spy();
+    db = {};
+
+    controller = new Controller(db);
+  });
+
+  afterEach(function () {
+    // In all cases next() must be called
+    sinon.assert.calledOnce(next);
+  });
+
+  it('getAllContacts returns all records in DB', function () {
+    const dummyDb = [ 123, 456 ];
+    db.records = dummyDb;
+
+    controller.getAllContacts(req, res, next);
+
+    sinon.assert.calledWith(res.send, dummyDb);
+  });
+
+  describe('getContactDetail', function () {
+    it('should return with error with invalid guid', function () {
+      req.params = { 
+        userId: 'invalid guid'
+      };
+
+      controller.getContactDetail(req, res, next);
+
+      const errorMessageBody = next.getCall(0).args[0].body;
+      expect(errorMessageBody.code).toBe('NotFoundError');
+      expect(errorMessageBody.message).toBe(`Record with id [${req.params.userId}] was not found in the database`);
+    });
+
+    it('should return with error if guid was not found in DB', function () {
+      req.params = { 
+        userId: validGuid
+      };
+
+      db.searchById = sinon.stub();
+
+      controller.getContactDetail(req, res, next);
+
+      const errorMessageBody = next.getCall(0).args[0].body;
+      expect(errorMessageBody.code).toBe('NotFoundError');
+      expect(errorMessageBody.message).toBe(`Record with id [${req.params.userId}] was not found in the database`);
+    });
+
+    it('should return the value if found', function () {
+      req.params = { 
+        userId: validGuid
+      };
+
+      db.searchById = sinon.stub().returns('item');
+
+      controller.getContactDetail(req, res, next);
+
+      sinon.assert.calledWith(res.send, 'item');
+    });
+  });
+
+  it('searchByName should use name value from request parameters', function () {
+    req.params = { 
+      name: 'Név Tibor'
+    };
+
+    db.searchByName = sinon.stub().returns([ 'item' ]);
+
+    controller.searchByName(req, res, next);
+
+    sinon.assert.calledWith(res.send, [ 'item' ]);
+  });
+
+  describe('addContact', function () {
+    let validRequestBody;
+
+    beforeEach(function () {
+      validRequestBody = {
+        userId: validGuid,
+        name: 'aasdf',
+        phoneNumber: '(234) 23424'
+      };
+
+      db.addContact = sinon.spy();
+    });
+
+    it('should return with error when GUID could not be parsed', function () {
+      const wrongUserId = 'blah blah';
+
+      req.body = JSON.stringify({ 
+        userId: wrongUserId
+      });
+
+      controller.addContact(req, res, next);
+
+      verifyBadRequestError(`Bad user id format [${wrongUserId}]`);
+    });
+
+    it('should return with error when name was not specified', function () {
+      req.body = JSON.stringify({ 
+        userId: validGuid
+      });
+
+      controller.addContact(req, res, next);
+
+      verifyBadNameErrorMessage();
+    });
+
+    it('should return with error when name was too short', function () {
+      req.body = JSON.stringify({ 
+        userId: validGuid,
+        name: 'a'
+      });
+
+      controller.addContact(req, res, next);
+
+      verifyBadNameErrorMessage();
+    });
+
+    it('should return with error when name are too long', function () {
+      req.body = JSON.stringify({ 
+        userId: validGuid,
+        name: '01234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789'
+      });
+
+      controller.addContact(req, res, next);
+
+      verifyBadNameErrorMessage();
+    });
+
+    it('should return with error when phone number was empty', function () {
+      req.body = JSON.stringify({ 
+        userId: validGuid,
+        name: 'aasdf'
+      });
+
+      controller.addContact(req, res, next);
+
+      verifyBadRequestError('Phone number cannot be empty.');
+    });
+
+    it('should return call DB with (id, name, phoneNumber) when everything was valid', function () {
+      validRequestBody.maliciousAdditionalField = {}
+      req.body = JSON.stringify(validRequestBody);
+
+      controller.addContact(req, res, next);
+
+      sinon.assert.calledOnce(db.addContact);
+      const callArgs = db.addContact.getCall(0).args[0];
+      
+      expect(callArgs.id.value).toBe(validGuid);
+      expect(callArgs.name).toBe('aasdf');
+      expect(callArgs.phoneNumber).toBe('(234) 23424');
+      expect(callArgs.maliciousAdditionalField).toNotExist();
+    });
+
+    it('should return 201 when everything was valid', function () {
+      req.body = JSON.stringify(validRequestBody);
+
+      controller.addContact(req, res, next);
+
+      sinon.assert.calledWith(res.send, 201);
+    });
+
+    function verifyBadRequestError(message) {
+      const errorMessageBody = next.getCall(0).args[0].body;
+      expect(errorMessageBody.code).toBe('BadRequestError');
+      expect(errorMessageBody.message).toBe(message);
+    }
+
+    function verifyBadNameErrorMessage() {
+      verifyBadRequestError('Name should be 3-100 long.');
+    }
+  })
+});
