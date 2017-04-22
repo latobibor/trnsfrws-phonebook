f36768b8ed07c9b298d0573b3a32901e6ee38e12
Tóth András <toth83andras@yahoo.com>
2017-04-22 21:32:53 +0200

    Fixed wrong binding.

--- backend/routes.js
+++ backend/routes.js
@@ -4,11 +4,11 @@ const Controller = require('./controller');
 const controller = new Controller(db);
 
 function setRoutes(server) {
-    server.get('/contacts', controller.getAllContacts);
-    server.get('/contact-details/:userId', controller.getContactDetail);
-    server.get('/search/:name', controller.searchByName);
+    server.get('/contacts', controller.getAllContacts.bind(controller));
+    server.get('/contact-details/:userId', controller.getContactDetail.bind(controller));
+    server.get('/search/:name', controller.searchByName.bind(controller));
 
-    server.post('/contact/', controller.addContact);
+    server.post('/contact/', controller.addContact.bind(controller));
 }
 
 module.exports = setRoutes;
